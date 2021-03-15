d3585a512c092b6d24846fbffedb678da771ab1c
rares <roman_rares93@yahoo.com>
2021-03-15 21:40:16 +0200

    finalize reactjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { PersonsTable } from "./PersonsTable";
+
+let persons = [
+  {
+      "id": "a123",
+      "firstName": "Raresa123",
+      "lastName": "Roman",
+      "github": "https://github.com/romanrares"
+  },
+  {
+      "id": "b654",
+      "firstName": "Nicolae",
+      "lastName": "Matei",
+      "github": "http://nmatei.github.io/"
+  }
+];
+
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
       <div>Search</div>
-      <p>table...</p>
+      <PersonsTable  persons={persons}/>
     </div>
   );
 }
---
+++ src/PersonsTable.js
@@ -0,0 +1,38 @@
+
+
+export const PersonsTable = (props) => (
+    <table id="list">
+        <thead>
+            <tr>
+                <th>FirstName</th>
+                <th>LastName</th>
+                <th>Links</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.persons.map((person, index) => (
+            <tr key={index}>
+                <td>{person.firstName}</td>
+                <td>{person.lastName}</td>
+                <td><a target="_blank" href={person.gitHub}>Github</a></td>
+                <td>
+                    <a href="#" className="delete-row" data-id="{person.id}">&#10006;</a>
+                    <a href="#" className="edit-row" data-id="{person.id}">&#9998;</a>
+                </td>
+            </tr>
+            ))}
+        </tbody>
+        <tfoot>
+            <tr>
+                <td><input type="text" required name="firstName" placeholder="Enter first name" /></td>
+                <td><input type="text" required name="lastName" placeholder="Enter last name" /></td>
+                <td><input type="text" required name="gitHub" placeholder="GitHub account" /></td>
+                <td>
+                    <button id="add-member">Save</button>
+                </td>
+            </tr>
+        </tfoot>
+
+    </table>
+);
