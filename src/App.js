50642233550276a543277a682fa10ac7f968f7b1
rares <roman_rares93@yahoo.com>
2021-03-22 20:04:29 +0200

    add Component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
+import { Component } from 'react';
 import './App.css';
 import { PersonsTable } from "./PersonsTable";
 
-let persons = [
-  {
-      "id": "a123",
-      "firstName": "Raresa123",
-      "lastName": "Roman",
-      "github": "https://github.com/romanrares"
-  },
-  {
-      "id": "b654",
-      "firstName": "Nicolae",
-      "lastName": "Matei",
-      "github": "http://nmatei.github.io/"
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      persons: []
+    }    
   }
-];
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        persons: [
+          {
+            "id": "a123",
+            "firstName": "Raresa123",
+            "lastName": "Roman",
+            "github": "https://github.com/romanrares"
+          },
+          {
+            "id": "b654",
+            "firstName": "Nicolae",
+            "lastName": "Matei",
+            "github": "http://nmatei.github.io/"
+          }
+        ]
+      })
+    }, 2000);
+  }
 
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <PersonsTable  persons={persons} border = {0}/>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Teams Networking</h1>
+        <div>Search</div>
+        <PersonsTable persons={this.state.persons} border={0} />
+      </div>
+    );
+  }
 }
 
 export default App;
